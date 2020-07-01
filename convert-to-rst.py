0fa881edb538c562eda300c6196d0c30b1ba4e9f
James Douglass <jamesdouglassusa@gmail.com>
2020-06-30 21:19:35 -0700

    Lots of work to make image paths with spaces in them work.

--- convert-to-rst.py
+++ convert-to-rst.py
@@ -1,4 +1,7 @@
 import sys
+import html
+import urllib.parse
+import re
 
 from bs4 import BeautifulSoup
 
@@ -9,27 +12,45 @@ def is_question(paragraph):
     except KeyError:
         return True
 
+def escape(text):
+    for specialchar in '$%&#_{}!':
+        text = text.replace(specialchar, f'\\{specialchar}')
+    return text
+
 
 def generate_latex(source_html_path):
     soup = BeautifulSoup(open(source_html_path).read(), 'html.parser')
     output_string = ""
     output_string += '\\documentclass[a4paper, twoside, 11pt]{article}\n'
+    output_string += '\\usepackage{graphicx}\n'
+    output_string += '\\usepackage[space]{grffile}\n'
     output_string += '\\begin{document}\n'
+
+    title = soup.h1.text.replace(' - ', ' \\textemdash{} ').strip()
 
     for index, paragraph in enumerate(soup.find_all('p')):
         if is_question(paragraph):
             if index > 0:
                 output_string += '\\newpage\n'
 
-            output_string += f'{{\\scshape {soup.h1.text} }}\n\n'
+            output_string += '\n'
+            output_string += f'{{\\scshape {title}}}\n\n'
             output_string += '\\bigskip\n'
-            output_string += f'{paragraph.text}\n'
+            output_string += f'{escape(paragraph.text)}\n'
+        elif paragraph.img:  # will be None if not here.
+            img_src = (urllib.parse.unquote(
+                html.unescape(paragraph.img['src'])))
+            import os
+            file_path, ext = os.path.splitext(img_src)
+            output_string += '\\begin{center}\n'
+            output_string += (
+                f'\\includegraphics[scale=1]{{./{{{file_path}}}{ext}}}\n')
+            output_string += '\\end{center}\n'
         else:
-            output_string += '\\bigskip\n'
-            output_string += '\\begin{enumerate}'
-            output_string += '\\itemsep0em\n'  # limit space between items
+            output_string += '\\bigskip\n\n'
+            output_string += '\\begin{enumerate}[itemsep=0em]\n'
             for line in paragraph.text.strip().split('\n'):
-                output_string += f'  \\item {line.strip()}\n'
+                output_string += f'  \\item {escape(line.strip())}\n'
             output_string += '\\end{enumerate}\n'
     output_string += '\\end{document}'
 
