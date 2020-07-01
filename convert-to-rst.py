2815b23356851b801c7c259065c6e65ee8f6a193
James Douglass <jamesdouglassusa@gmail.com>
2020-06-30 20:09:26 -0700

    Refactor to append to a string.

--- convert-to-rst.py
+++ convert-to-rst.py
@@ -12,27 +12,29 @@ def is_question(paragraph):
 
 def generate_latex(source_html_path):
     soup = BeautifulSoup(open(source_html_path).read(), 'html.parser')
-    print(r'\documentclass[a4paper, twoside, 11pt]{article}' + '\n')
-    print(r'\begin{document}' + '\n')
+    output_string = ""
+    output_string += '\\documentclass[a4paper, twoside, 11pt]{article}\n'
+    output_string += '\\begin{document}\n'
 
     for index, paragraph in enumerate(soup.find_all('p')):
-
         if is_question(paragraph):
             if index > 0:
-                print('\\newpage\n')
+                output_string += '\\newpage\n'
 
-            print(r'{\scshape ' + soup.h1.text + '}\n\n')
-            print(r'\bigskip' + '\n')
-            print(f'{paragraph.text}\n')
+            output_string += f'{{\\scshape {soup.h1.text} }}\n\n'
+            output_string += '\\bigskip\n'
+            output_string += f'{paragraph.text}\n'
         else:
-            print('\\bigskip\n')
-            print('\\begin{enumerate}')
-            print('\\itemsep0em\n')  # limit space between items
+            output_string += '\\bigskip\n'
+            output_string += '\\begin{enumerate}'
+            output_string += '\\itemsep0em\n'  # limit space between items
             for line in paragraph.text.strip().split('\n'):
-                print(f'  \\item {line.strip()}')
-            print('\\end{enumerate}\n')
-    print('\\end{document}')
+                output_string += f'  \\item {line.strip()}\n'
+            output_string += '\\end{enumerate}\n'
+    output_string += '\\end{document}'
+
+    return output_string
 
 
 if __name__ == '__main__':
-    generate_latex(sys.argv[1])
+    print(generate_latex(sys.argv[1]))
