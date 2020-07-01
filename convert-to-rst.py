081c6b37e6693b9cd24b7bb68df7d0284de4a27e
James Douglass <jamesdouglassusa@gmail.com>
2020-06-30 19:41:32 -0700

    Cleaning up script.

--- convert-to-rst.py
+++ convert-to-rst.py
@@ -1,19 +1,5 @@
-import textwrap
-import string
-
 from bs4 import BeautifulSoup
 
-
-PAGETOP = """
-    \documentclass[a4paper, twoside, 11pt]{article}
-    \pagestyle{empty}
-    \begin{document}
-    {\scshape Chapter 1 - Introduction to Statistics}
-
-    \textbf{Homework 1}
-
-    \bigskip
-"""
 
 def is_question(paragraph):
     try:
@@ -21,12 +7,10 @@ def is_question(paragraph):
     except KeyError:
         return True
 
+
 if __name__ == '__main__':
     soup = BeautifulSoup(open('Homework Chapter 1.html').read(), 'html.parser')
-    #print(str(soup.h1.text))
-    #print('-'*len(str(soup.h1.text)))
     print(r'\documentclass[a4paper, twoside, 11pt]{article}' + '\n')
-    print(r'\pagestyle{empty}' + '\n')
     print(r'\begin{document}' + '\n')
 
     for index, paragraph in enumerate(soup.find_all('p')):
@@ -39,13 +23,10 @@ if __name__ == '__main__':
             print(r'\bigskip' + '\n')
             print(f'{paragraph.text}\n')
         else:
-            #print('\\begin{itemize}')
             print('\\bigskip\n')
+            print('\\begin{enumerate}')
+            print('\\itemsep0em\n')  # limit space between items
             for line in paragraph.text.strip().split('\n'):
-                #line = ' '.join(line.split(' ')[1:])  # Strip leading index
-                print(f'{line.strip()}\n')
-                #print(f'  \\item {line}')
-            #print('\\end{itemize}\n')
+                print(f'  \\item {line.strip()}\n')
+            print('\\end{enumerate}\n')
     print('\\end{document}')
-
-
